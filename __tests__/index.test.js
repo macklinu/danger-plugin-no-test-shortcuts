0c7ce354f8ae5a1321f85717c3f2c92af20c3c94
Macklin Underdown <macklinu@gmail.com>
2017-05-03 13:16:51 -0400

    test(noTestShortcuts): Add tests for failing when .only() is present

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -47,4 +47,35 @@ describe('noTestShortcuts()', () => {
       )
     })
   })
+  describe('test function names', () => {
+    ;['describe', 'context', 'it', 'test'].forEach(testFn => {
+      const MOCK_FILE_INFO = {
+        'tests/describe.test.js': 'describe.only("some test");',
+        'tests/test.test.js': 'test.only("some test");',
+        'tests/it.test.js': 'it.only("some test");',
+        'tests/context.test.js': 'context.only("some test");'
+      }
+      beforeEach(() => {
+        require('fs').__setMockFiles(MOCK_FILE_INFO)
+        global.fail = jest.fn()
+      })
+      afterEach(() => {
+        global.danger = undefined
+        global.fail = undefined
+      })
+      it(`fails the build when a test contains ${testFn}.only`, () => {
+        global.danger = {
+          git: {
+            created_files: [`tests/${testFn}.test.js`],
+            modified_files: []
+          }
+        }
+
+        noTestShortcuts()
+
+        expect(global.fail)
+          .toHaveBeenCalledWith(`an \`only\` was left in tests: tests/${testFn}.test.js`)
+      })
+    })
+  })
 })
