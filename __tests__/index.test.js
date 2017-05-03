4a39b294753c95c9e77c4a0cebeffae297d3eaa4
Macklin Underdown <macklinu@gmail.com>
2017-05-03 12:54:06 -0400

    feat(noTestShortcuts): Create noTestShortcuts plugin function with testFilePredicate option
    BREAKING CHANGE: This commit replaces index.js with an exported function, which also contains a
    single configuration option

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,50 @@
-test('placeholder test', () => {
-    expect(true).toBe(true)
+import noTestShortcuts from '../'
+
+jest.mock('fs')
+
+describe('noTestShortcuts()', () => {
+  describe('config: testFilePredicate', () => {
+    const MOCK_FILE_INFO = {
+      'path/to/tests/index.test.js': 'describe.only("some test");',
+      'tests/subdirectory/myTest.js': 'describe.only("My Test");'
+    }
+    beforeEach(() => {
+      require('fs').__setMockFiles(MOCK_FILE_INFO)
+      global.fail = jest.fn()
+    })
+    afterEach(() => {
+      global.danger = undefined
+      global.fail = undefined
+    })
+    it('honors test file predicate', () => {
+      global.danger = {
+        git: {
+          created_files: ['path/to/tests/index.test.js'],
+          modified_files: []
+        }
+      }
+
+      noTestShortcuts({
+        testFilePredicate: path => path.endsWith('.test.js')
+      })
+
+      expect(global.fail).toHaveBeenCalledWith(
+        'an `only` was left in tests: path/to/tests/index.test.js'
+      )
+    })
+    it('defaults to the tests/ directory', () => {
+      global.danger = {
+        git: {
+          created_files: ['tests/subdirectory/myTest.js'],
+          modified_files: []
+        }
+      }
+
+      noTestShortcuts()
+
+      expect(global.fail).toHaveBeenCalledWith(
+        'an `only` was left in tests: tests/subdirectory/myTest.js'
+      )
+    })
+  })
 })
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// TODO
+import {readFileSync} from 'fs'
+
+export default function noTestShortcuts (
+  {testFilePredicate = path => path.startsWith('tests')} = {}
+) {
+  const newOrModifiedFiles = danger.git.modified_files.concat(
+    danger.git.created_files
+  )
+  const newOrModifiedTests = newOrModifiedFiles.filter(testFilePredicate)
+  newOrModifiedTests.forEach(file => {
+    const content = readFileSync(file).toString()
+    if (
+      content.includes('it.only') ||
+      content.includes('describe.only') ||
+      content.includes('context.only') ||
+      content.includes('test.only')
+    ) {
+      fail(`an \`only\` was left in tests: ${file}`)
+    }
+  })
+}
